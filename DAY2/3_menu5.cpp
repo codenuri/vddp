23593e9f6ddaddbb21186c00597e236c49b42855
ecourse <smkang@codenuri.co.kr>
2024-07-31 11:06:38 +0900

    Update 3_menu5.cpp

--- DAY2/3_menu5.cpp
+++ DAY2/3_menu5.cpp
@@ -95,21 +95,33 @@ int main()
 	PopupMenu* pm2 = new PopupMenu("해상도 변경");
 
 	root->add(pm1);
-	root->add(pm2);
+//	root->add(pm2);
+	pm1->add(pm2);	// 메뉴 구조 변경
 
 	pm1->add( new MenuItem("RED",   11));
 	pm1->add( new MenuItem("GREEN", 12));
 	pm1->add( new MenuItem("BLUE",  13));
+	pm1->add( new MenuItem("WHITE",  14));
 
 	pm2->add( new MenuItem("HD",   21));
 	pm2->add( new MenuItem("FHD",  22));
 	pm2->add( new MenuItem("UHD",  23));
 
 	// 이제 시작하려면 ???
-
+	root->command();
 }
 
+// 객체지향 프로그램에서 "프로그램이란 ?"
 
+// 1. 객체를 생성하고
+// 2. 객체간의 관계를 설정하고
+// 3. 객체간의 메세지를 주고 받는 과정이다(서로의 멤버 함수를 호출 한다는 의미)
+// 4. 프로그램의 기본 단위는 "함수" 가 아니라 "클래스" 이다.
+
+// 장점 : 유지보수가 쉽고 유연하다.
+// 단점 : 메모리 사용량 증가(필요없는 멤버 데이타, 가상함수 테이블등)
+//       C 보다 느리다.
+//       어렵다. (많은 경험 필요)
 
 
 
