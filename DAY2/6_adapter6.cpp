87d63214165b77be7e28b0b9c03d758c12364102
ecourse <smkang@codenuri.co.kr>
2024-07-31 15:28:08 +0900

    Create 6_adapter6.cpp

---
+++ DAY2/6_adapter6.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <list>
+#include <vector>
+#include <deque>
+
+// 핵심 :  adapter 패턴과 Policy Base Design 의 결합
+
+template<typename T, typename C = std::deque<T> >
+class stack 
+{
+	C c;
+public:
+	inline void push(const T& e)  { c.push_back(e);}
+	inline void pop()             { c.pop_back();}
+	inline T&   top()             { return c.back();}
+};
+
+#include <stack> // C++ 표준 stack
+				 // 이 안의 코드가 위와 거의 동일하게 되어 있습니다.
+				 // 흔히 "stack adapter" 라고 부릅니다.
+int main()
+{	
+	stack<int, std::vector<int>> s1;
+	stack<int, std::list<int>> s2;
+
+	stack<int> s;
+	s.push(10);
+
+}
