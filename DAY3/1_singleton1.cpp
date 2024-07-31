9caa69731f4a81ab5c5d92b014bd4f99ecfe2de5
ecourse <smkang@codenuri.co.kr>
2024-08-01 08:41:25 +0900

    Update 1_singleton1.cpp

--- DAY3/1_singleton1.cpp
+++ DAY3/1_singleton1.cpp
@@ -4,14 +4,35 @@
 // 싱글톤 : 오직 한개의 객체만 존재하고
 //         어디서든지 동일한 방법으로 접근 가능한 것
 
+// => 결국 전역변수와 유사한 특징을 가지게 됩니다.
+// => 전역변수는 좋은 코딩 스타일이 아닙니다.
+// => 그래서 싱글톤 패턴에 대한 비판도 많이 있습니다.
+
 class Cursor
 {
+	// 규칙 #1. 생성자를 private 에
+private:
+	Cursor() {}
+
+	// 규칙 #2.
+
+
+	// 규칙 #3. 오직 한개의 객체를 만들어서 반환하는 static 멤버 함수
+public:
+	static Cursor& get_instance()
+	{
+		static Cursor instance;  // static 지역변수가 핵심
+		return instance;
+	}
 };
-
-
 int main()
 {
-	Cursor c1, c2;
+	Cursor& c1 = Cursor::get_instance();
+	Cursor& c2 = Cursor::get_instance();
+
+	std::cout << &c1 << std::endl;
+	std::cout << &c2 << std::endl;
+
 }
 
 
