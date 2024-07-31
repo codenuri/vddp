2a6c6363cced7e27b07c363e707d9e92d62c8677
ecourse <smkang@codenuri.co.kr>
2024-08-01 08:56:20 +0900

    Create 1_singleton3.cpp

---
+++ DAY3/1_singleton3.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <mutex>
+
+// 동기화!!
+
+class Cursor
+{
+private:
+	Cursor() {}
+	Cursor(const Cursor&) = delete; 
+	Cursor& operator=(const Cursor&) = delete;
+
+	static Cursor* sinstance;	
+	static std::mutex mtx;
+public:
+	static Cursor& get_instance()
+	{
+		mtx.lock();
+		
+		if ( sinstance == nullptr )
+			sinstance = new Cursor;
+		
+		mtx.unlock();
+
+		return *sinstance;
+	}
+};
+Cursor* Cursor::sinstance = nullptr; 
+std::mutex Cursor::mtx;
+
+
+
+
+int main()
+{
+	Cursor& c1 = Cursor::get_instance();
+	Cursor& c2 = Cursor::get_instance();
+
+	std::cout << &c1 << std::endl;
+	std::cout << &c2 << std::endl;
+
+}
+
+
+
+
+
+
+
+
