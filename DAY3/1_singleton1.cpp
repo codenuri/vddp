5c7ac56d6f77cf6e97c1b8494cd20bfb81312f22
ecourse <smkang@codenuri.co.kr>
2024-08-01 08:19:22 +0900

    Create 1_singleton1.cpp

---
+++ DAY3/1_singleton1.cpp
@@ -0,0 +1,23 @@
+// 7_싱글톤1 - 123 page
+#include <iostream>
+
+// 싱글톤 : 오직 한개의 객체만 존재하고
+//         어디서든지 동일한 방법으로 접근 가능한 것
+
+class Cursor
+{
+};
+
+
+int main()
+{
+	Cursor c1, c2;
+}
+
+
+
+
+
+
+
+
