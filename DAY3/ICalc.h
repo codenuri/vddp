eef26a8d5c8e775c07b97f57a7d673a48eec65cd
ecourse <smkang@codenuri.co.kr>
2024-08-01 14:43:57 +0900

    Update ICalc.h

--- DAY3/ICalc.h
+++ DAY3/ICalc.h
@@ -7,3 +7,22 @@ struct ICalc
 	virtual int Sub(int, int ) = 0;
 	virtual ~ICalc() {}
 };
+
+
+
+ICalc* load_proxy()
+{
+	void* addr = ec_load_module("CalcProxy.dll");
+						// windows : LoadLibrary()
+						// linux   : dlopen()
+
+	using F = ICalc*(*)();
+
+	F f = (F)ec_get_function_address(addr, "create");
+						// windows : GetProcAddress()
+						// linux   : dlsym()
+
+	ICalc* calc = f();
+
+	return calc;
+}
