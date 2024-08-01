85e7a4238b13e2bbd9e839c73ef3131a0e9d83bc
ecourse <smkang@codenuri.co.kr>
2024-08-01 10:23:21 +0900

    Update 3_Factory2.cpp

--- DAY3/3_Factory2.cpp
+++ DAY3/3_Factory2.cpp
@@ -15,12 +15,23 @@ public:
 	void draw() override { std::cout << "draw Rect" << std::endl; }
 
 	// 자신의 객체를 만드는 static 멤버 함수가 아주 유용하게 사용됩니다.
+	// => Java 진영에서는 "static factory method" 라고 합니다.
+	// => GoF's 패턴에는 없는 기법
 	static Shape* create() { return new Rect; }
 };
 
 // Rect 객체를 만드는 2가지 방법
-// 1. Rect* rc = new Rect;
-// 2. Rect* rc = Rect::create();
+// 1. Rect* rc = new Rect;		// 객체를 만들기 위해 클래스 이름을 알아야 합니다.
+// 2. Rect* rc = Rect::create();// 클래스 이름을 몰라도 함수의 주소만 알면 객체 생성가능.
+
+// C++ 에서는 "클래스"를 자료구조에 보관할수 없습니다.
+// v.push_back("Rect"); 	// 클래스가 아닌 문자열을 보관한것!!
+							// 문자열로는 객체 생성이 안됩니다.
+
+// 함수포인터는 자료구조에 보관 가능합니다
+// v.push_back(&Rect::create); // 보관된 주소로 객체 생성 가능
+
+
 
 
 
@@ -37,13 +48,28 @@ public:
 class ShapeFactory
 {
 	MAKE_SINGLETON(ShapeFactory)
+
+	using CREATOR = Shape*(*)();
+
+	std::map<int, CREATOR> create_map;
 public:
+
+	void register_shape(int key, CREATOR c)
+	{
+		create_map[key] = c;
+	}
+
 	Shape* create(int key)
 	{
 		Shape* s = nullptr;
 
-		if      ( key == 1) s = new Rect;
-		else if ( key == 2) s = new Circle;
+		auto it = create_map.find(key);
+
+		if ( it != create_map.end() )
+		{
+			s = it->second(); 	// it->second 는 value 즉, 
+								// 등록된 생성 함수
+		}
 
 		return s;
 	}
