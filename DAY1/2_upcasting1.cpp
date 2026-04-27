16d6806e84d108e2db72dc0eeeab26723b728ca7
ecourse <smkang@codenuri.co.kr>
2026-04-27 10:20:49 +0900

    Add files via upload

--- DAY1/2_upcasting1.cpp
+++ DAY1/2_upcasting1.cpp
@@ -28,9 +28,25 @@ int main()
 	// 핵심 #3. p3 를 사용해서는 
 	// => Dog 고유의 멤버에 접근할수없다.
 	// => Animal 로 부터 상속된 멤버만 접근 가능
-
+	// 이유가 중요합니다.
+	// C++ 은 static type check 언어 이므로
+	// static  type check : 컴파일 시간에 타입을 확인하는 언어
+	//						C++, C#, Java, Rust 
+	// dynamic type check : 실행 시간에 타입을 확인하는 언어
+	//						Python
+	
+	// p3 는 Animal* 타입 이므로
+	// => p3 를 따라가면 Animal 멤버인 age 가 있다는 사실은 컴파일러가 보장 가능
+	// => 하지만 Dog 고유 멤버인 color 가 있다고는 보장할수 없다.
 	p3->age = 10;   // ok
 	p3->color = 10; // error
+
+	// 핵심 #4. p3 를 사용해서 Dog 고유 멤버에 접근하려면
+	// => 컴파일러가 보장할수 없으므로
+	// => 사용자가 책임지고 접근하겠다고 표시해야 합니다.
+	//    캐스팅
+	static_cast<Dog*>(p3)->color = 10;
+	
 }
 
 
