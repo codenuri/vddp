bba7fe026951a05e16f2004d360faf603c5708e2
ecourse <smkang@codenuri.co.kr>
2026-04-27 09:45:35 +0900

    Add files via upload

--- DAY1/1_생성자3.cpp
+++ DAY1/1_생성자3.cpp
@@ -15,10 +15,26 @@ public:
 // 3. Student 생성자 만들어서 초기화해 보세요
 // 4. main 에서 Student 객체 생성해 보세요
 
+class Student : public Person
+{
+	int id;
+public:
+	// 사용자 코드					// 컴파일러 변경한 코드
+	//Student(int id) : id(id) { }	// Student(int id) : Person(), id(id) { }
+									//                    ^ 이 부분에서 에러
 
+	// 해결책 : Person 의 생성자를 명시적으로 호출해야 합니다.
+	// 이름, 나이, id 를 인자로 받아서
+	// 이름, 나이 => 기반 클래스 생성자에 전달
+	// id => 자신의 멤버를 초기화
+	Student(const std::string& name, int age, int id) 
+		: Person(name, age), id(id) {}
+};
 
 int main()
 {
+	Student s1("Bob", 20, 12345);
+
 	Person p1("Alice", 30);
 
 //	Person p1;	// 이렇게 객체를 만들수 있다는 것이 좋을까요 ? (초기화 되지 않은 객체)
