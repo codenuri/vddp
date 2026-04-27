7d58e34cda4a2bae70b47e1ce74ded53c9554bbf
ecourse <smkang@codenuri.co.kr>
2026-04-27 11:02:49 +0900

    Add files via upload

---
+++ DAY1/3_가상함수1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+class Animal
+{
+public:
+	void cry() { std::cout << "Animal Cry" << std::endl; } 
+};
+class Dog : public Animal
+{
+public:
+	// function override : 기반 클래스 멤버 함수를 파생 클래스가 다시 구현을 제공하는 것
+	void cry() { std::cout << "Dog Cry" << std::endl; } 
+};
+int main()
+{
+	Dog d;
+	Animal* p = &d; 
+
+	// p 자체의 타입 : Animal*
+    // p 가 가리키는 객체의 타입 : Dog
+
+	// 아래 코드는 어떤 함수를 호출할까요 ?
+	p->cry();	// 1. Animal Cry
+				// 2. Dog Cry
+}
+
+
