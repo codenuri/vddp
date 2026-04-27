50a484ac04a6f736a9832730af582eb3f82fcf26
ecourse <smkang@codenuri.co.kr>
2026-04-27 10:45:22 +0900

    Add files via upload

---
+++ DAY1/2_upcasting3.cpp
@@ -0,0 +1,15 @@
+﻿// upcasting2.cpp
+#include <vector>
+
+class Animal {};
+class Dog : public Animal {};
+
+int main()
+{
+	std::vector<Dog*>    v1;	// Dog 타입의 객체만 보관 가능한 컨테이너
+	std::vector<Animal*> v2;    // 동종을 보관 가능한 컨테이너
+								// (동일 기반 클래스를 사용하는 타입들.. 모든 동물)
+
+	// upcasting 장점(활용) #1.
+	// => 동종을 보관하는 컨테이너 만들기 							
+}
