4aef6d6b8e258d3e7663778173e94d54aec09d6d
ecourse <smkang@codenuri.co.kr>
2026-04-27 13:26:44 +0900

    Add files via upload

--- DAY1/5_도형편집기4.cpp
+++ DAY1/5_도형편집기4.cpp
@@ -21,6 +21,11 @@ class Circle : public Shape
 public:
 	void draw() { std::cout << "draw Circle\n"; }
 };
+class Triangle : public Shape
+{
+public:
+	void draw() { std::cout << "draw Triangle\n"; }
+};
 int main()
 {
 	std::vector<Shape*> v; 
@@ -51,8 +56,24 @@ int main()
 				{
 					c->draw();
 				}
+				
+				// 새로운 도형(Triangle) 이 추가되면 아래 코드도 추가되어야 합니다.
+				if ( Triangle* t = dynamic_cast<Triangle*>(s); Triangle != nullptr )
+				{
+					t->draw();
+				}			
+
 			}
 		}
 	}
 }
+// 위 방법이 좋은 디자인 일까요 ?
+// => 나쁜 디자인!!
 
+// 위 코드는 OCP 를 만족하지 않은 코드
+// 기능 확장에 열려있고(Open, 새로운 클래스/모듈이 추가 되어도)
+// 코드 수정에는 닫혀 있어야(Close, 기존 코드는 수정되지 않아야 한다.)
+// 는 원칙(Principle)
+
+// Open-Close principle 이라는 규칙.
+// 위 코드는 OCP 를 만족할수 없는 좋지 않은 디자인
