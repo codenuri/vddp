b87b2aacb1a965b117bf6d79b445a42888b7cfc8
ecourse <smkang@codenuri.co.kr>
2026-04-27 15:42:41 +0900

    Add files via upload

---
+++ DAY1/6_Edit1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+#include <conio.h>
+// GUI 프로그램에서 사용하는 Widget(Control) 를 생각해 봅시다.
+
+class Edit
+{
+	std::string data;
+public:
+	std::string get_data()
+	{
+		std::cin >> data;
+		return data;
+	}
+};
+
+int main()
+{
+	Edit e;		// 입력창(Edit) 생성
+				// => 입력된 데이타를 꺼내려면 get_data() 호출
+
+	while (1)
+	{
+		std::cout << e.get_data() << std::endl;
+	}
+}
+
+
