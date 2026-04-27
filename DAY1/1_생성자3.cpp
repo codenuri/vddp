df84cae5accdbed3a3f648db1b73490373e5b887
ecourse <smkang@codenuri.co.kr>
2026-04-27 09:38:03 +0900

    Add files via upload

--- DAY1/1_생성자3.cpp
+++ DAY1/1_생성자3.cpp
@@ -9,6 +9,13 @@ class Person
 public:
 	Person(const std::string& name, int age) : name(name), age(age) {}
 };
+// 아래 주석대로 만들어 보세요
+// 1. Person 에서 상속 받는 Student 만들어 보세요
+// 2. Student 에 int id 멤버 추가해 보세요
+// 3. Student 생성자 만들어서 초기화해 보세요
+// 4. main 에서 Student 객체 생성해 보세요
+
+
 
 int main()
 {
