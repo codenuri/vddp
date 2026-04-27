3a53408da48263d48249f90d6df4c90c00676363
ecourse <smkang@codenuri.co.kr>
2026-04-27 10:14:19 +0900

    Add files via upload

---
+++ DAY1/2_upcasting1.cpp
@@ -0,0 +1,45 @@
+﻿class Animal
+{
+public:
+	int age;
+};
+class Dog : public Animal
+{
+public:
+	int color;
+};
+int main()
+{
+	Dog d;
+	Dog* p1 = &d; 		// ok
+//	int* p2 = &d; 		// error
+
+	// 핵심 #1. upcasting 개념
+	// => 기반 클래스 타입의 포인터(참조)로 파생 클래스의 객체 주소를 담을수 있다
+	// => 가능한 이유는 "메모리 그림" 을 생각하세요
+	Animal* p3 = &d; 	// ok
+
+	// 핵심 #2. 컴파일 시간에 컴파일러는 p3 가 가리키는 객체의 타입은 알수 없다
+	// => p3는 변수 이므로 실행시간에 변경될수 있으므로
+	// => 아래 예제 참고
+//	Animal a;
+//	if ( 사용자 입력 == 1 ) p3 = &a;
+
+	// 핵심 #3. p3 를 사용해서는 
+	// => Dog 고유의 멤버에 접근할수없다.
+	// => Animal 로 부터 상속된 멤버만 접근 가능
+
+	p3->age = 10;   // ok
+	p3->color = 10; // error
+}
+
+
+
+
+
+
+
+
+
+
+
