6150fff1028557694bb66b903f125c21e03be5e1
ecourse <smkang@codenuri.co.kr>
2026-04-28 13:19:05 +0900

    Add files via upload

--- DAY2/4_decorator2.cpp
+++ DAY2/4_decorator2.cpp
@@ -13,23 +13,19 @@ public:
 // 포함을 사용한 기능의 추가 : 객체에 기능 추가
 class Emoticon 
 {
-	PhotoSticker* orign; // [핵심] 값이 아닌 포인터 또는 참조
+	PhotoSticker* origin; // [핵심] 값이 아닌 포인터 또는 참조
 						 // 의도 : 이미 생성되어 있는 객체를 가리키겠다는 것
 public:
 
-	Emoticon(PhotoSticker* ps) : orign(ps){}
-
+	Emoticon(PhotoSticker* ps) : origin(ps){}
 	
 	void draw()	
 	{ 
-		std::cout << "&&&&&&&&&&&&&&&&&&&&\n"; 
-		PhotoSticker::draw();					
+		std::cout << "&&&&&&&&&&&&&&&&&&&&\n";  // 기능 추가
+		origin->draw();					
 		std::cout << "&&&&&&&&&&&&&&&&&&&&\n";  
 	}
 };
-
-
-
 
 int main()
 {
@@ -37,6 +33,9 @@ int main()
 	ps.take();	
 	ps.draw();	
 
-
+	Emoticon e(&ps); // e 는 이미 존재 하는 ps 에 기능을 추가하는 기능 추가 객체!!
+					 // 사진 자체는 이미 ps 객체가 촬영했고
+					 // e 가 하는 일은 ps 에 기능을 추가하는 것
+	e.draw();
 }
 
