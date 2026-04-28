907b14c83e7e805876f557910cf0b872e47c39f2
ecourse <smkang@codenuri.co.kr>
2026-04-28 16:07:54 +0900

    Add files via upload

--- DAY2/5_adapter5.cpp
+++ DAY2/5_adapter5.cpp
@@ -15,11 +15,31 @@ public:
 	void pop()            { c.pop_back(); }
 	T&   top()            { return c.back();}
 };
-
 int main()
 {
 	stack<int> s;
 	s.push(10);
-	
-	s.push_front(10); // error. stack 에는 이런 멤버 함수가 없다!
+//	s.push_front(10); // error. stack 에는 이런 멤버 함수가 없다!
 }
+
+// 핵심 : adaptor 같은 디자인 패턴을 사용시
+// 기반 클래스 멤버를 파생 클래스 객체에서도 외부에 노출하려면 상속
+// 기반 클래스 멤버를 파생 클래스 내부에서만 사용한다면 포함
+
+// 그러면 "private 상속" 은 언제 사용하나요 ?
+
+// 아래 A, B 는 모두 Sample 의 멤버 함수를 모두 내부적으로만 사용하는 코드 입니다.
+/*
+class A : private Sample 
+{
+	// Sample 에 가상함수가 있다면 재정의할 기회가 있다.
+public:
+	void sample_method() override {}
+};
+
+class B 
+{
+	Sample s;
+	// Sample 에 가상함수가 있다면 재정의할 기회가 없다.
+};
+*/
