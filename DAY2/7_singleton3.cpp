5e892af3c0a329fe3cc3583dad48a6dbc9fb8d22
ecourse <smkang@codenuri.co.kr>
2026-04-28 17:01:16 +0900

    Add files via upload

---
+++ DAY2/7_singleton3.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+// 이번 예제는 "스레드 동기화"
+
+class Cursor
+{
+private:
+	Cursor() {}
+	
+	Cursor(const Cursor&) = delete;				
+	Cursor& operator=(const Cursor&) = delete;	
+
+	inline static Cursor* instance = nullptr;
+public:
+	static Cursor& get_instance() 
+	{
+		if ( instance == nullptr )
+			instance = new Cursor;
+
+		return *instance;
+	}
+};
+
+
+int main()
+{
+	Cursor& c1 = Cursor::get_instance();
+	Cursor& c2 = Cursor::get_instance();
+}
+
