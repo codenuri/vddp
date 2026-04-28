b8c9d7a1693ea82eb62c7ddd492eda2cec17ec73
ecourse <smkang@codenuri.co.kr>
2026-04-28 12:54:35 +0900

    Add files via upload

---
+++ DAY2/monitor.h
@@ -0,0 +1,19 @@
+#include <iostream>
+
+class Monitor 
+{
+	int brightness = 80;
+	int resolution = 1920;
+public:
+	void set_brightness(int value) { brightness = value; }
+	void set_resolution(int value) { resolution = value; }
+
+	int get_brightness() const { return brightness;}
+	int get_resolution() const { return resolution;}
+
+	void info() const 
+	{
+		std::cout << "brightness : " << brightness << std::endl;
+		std::cout << "resolution : " << resolution << std::endl;
+	}
+};
