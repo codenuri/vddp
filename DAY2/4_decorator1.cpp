d55a82043fd769a43ecb00265b26d1675c1600d0
ecourse <smkang@codenuri.co.kr>
2026-04-28 13:08:01 +0900

    Add files via upload

--- DAY2/4_decorator1.cpp
+++ DAY2/4_decorator1.cpp
@@ -19,13 +19,35 @@ public:
 		PhotoSticker::draw();					// 기존 기능 사용		
 		std::cout << "&&&&&&&&&&&&&&&&&&&&\n";  // 이모티콘 그림
 	}
-
 };
-
+// 사진에 액자 그림을 추가하는 클래스
+class Frame : public PhotoSticker
+{
+public:
+	void draw()	
+	{ 
+		std::cout << "====================\n";  
+		PhotoSticker::draw();					
+		std::cout << "====================\n";  
+	}
+};
 
 int main()
 {
 	PhotoSticker ps;
 	ps.take();	// 사진 촬영
 	ps.draw();	// 사진 출력(인화)
+
+	Emoticon e;
+	e.take();
+	e.draw(); // 사진 출력 + 이모티콘 출력
+
+	Frame f;
+	f.take();
+	f.draw();	
 }
+
+// 위 코드는 상속을 사용해서 기능을 추가한 코드 입니다.
+// => 아래 중에서 어떤 것일까요 ?
+// 1. 객체에 기능 추가 ?
+// 2. 클래스에 기능 추가 ?
