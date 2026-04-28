aa97c58c38728ee0889e6ef5a95190134a1f225e
ecourse <smkang@codenuri.co.kr>
2026-04-28 15:23:55 +0900

    Add files via upload

---
+++ DAY2/5_adapter2-1.cpp
@@ -0,0 +1,29 @@
+
+// 디자인 패턴을 학습하다 보면, 다양한 패턴이 유사해 보이는 경우가 있습니다.
+// => 아래 2개 클래스는 구현이 유사합니다.
+
+class ZipDecorator : public Stream 
+{
+	Stream* origin; 
+public:
+	ZipDecorator(Stream* s) : origin(s) {}
+
+	void write(const std::string& s)
+	{
+		std::string data = "[ " + s + " ] 압축됨"; // [ hello ] 압축된
+		origin->write(data);
+	}
+};
+
+
+class ObjectAdapter : public Shape 
+{
+	TextView* origin; 
+public:
+	ObjectAdapter(TextView* s) : origin(s) {}
+
+	void draw() override 
+	{
+		origin->show();
+	}
+};
