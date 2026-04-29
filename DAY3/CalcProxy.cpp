17ba83fa894a721b885538f59487239810bd38b6
ecourse <smkang@codenuri.co.kr>
2026-04-29 16:27:04 +0900

    Add files via upload

--- DAY3/CalcProxy.cpp
+++ DAY3/CalcProxy.cpp
@@ -1,16 +1,29 @@
 #define USING_GUI
 #include "cppmaster.h"
 #include "ICalc.h"
+#include <atomic>	// 멀티 스레드 안전한 변수 만들때 사용
 
 class Calc : public ICalc
 {
 	int server;
+	std::atomic<int> refcnt = 0;
 public:
+	void AddRef() { ++refcnt; }
+	void Release() { if (--refcnt == 0) delete this; }
+
+	~Calc() { std::cout << "Calc Proxy 객체가 파괴되었습니다." << std::endl; }
+
+
+
 	Calc() { server = ec_find_server("Calc"); }
 
 	int Add(int a, int b) { return ec_send_server(server, 1, a, b); }
 	int Sub(int a, int b) { return ec_send_server(server, 2, a, b); }
 };
+
+
+
+
 
 extern "C" __declspec(dllexport)
 ICalc* create()
--- DAY3/ICalc.h
+++ DAY3/ICalc.h
@@ -1,12 +1,41 @@
 #pragma once
 
-// ICalc.h 헤더로 분리
-struct ICalc
+// ICalc.h 
+
+
+// 참조계수 기반으로 객체의 수명을 관리 한다면
+// 참조계수 관련 함수는 인터페이스에도 있어야 합니다.
+
+// 서버의 종류에 관계 없이 참조 계수 함수는 항상 동일 합니다.
+struct IRefCount
 {
+	virtual void AddRef() = 0;
+	virtual void Release() = 0;
+	virtual ~IRefCount() {}
+};
+
+
+struct ICalc : public IRefCount
+{
+//	virtual void AddRef() = 0;
+//	virtual void Release() = 0;
+
 	virtual int Add(int a, int b) = 0;
 	virtual int Sub(int a, int b) = 0;
 	virtual ~ICalc() {}
 };
+
+// LedServer 에 대한 Proxy
+struct ILedServer : public IRefCount
+{
+	virtual int LedOn() = 0;
+	virtual int LedOff() = 0;
+	virtual ~ILedServer() {}
+};
+
+
+
+
 
 
 
