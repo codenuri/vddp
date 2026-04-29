bbeae610c871b94be201624a490de5f59a518a75
ecourse <smkang@codenuri.co.kr>
2026-04-29 15:51:03 +0900

    Add files via upload

--- DAY3/CalcProxy.cpp
+++ DAY3/CalcProxy.cpp
@@ -25,3 +25,11 @@ ICalc* create()
 // 새로운 Proxy DLL 을 만들어도 
 // => DLL 이름은 동일하게 유지하고
 // => DLL 내부에서 create() 함수 이름도 동일하게 유지. 
+
+// 이 소스를 DLL 로 빌드 하는법
+// #1. 윈도우 시작 버튼을 누르고 "developer command prompt" 입력해서 나타나는 창 실행
+// #2. 현재 소스가 있는 곳으로 이동
+// #3. cl /LD CalcProxy.cpp   로 컴파일.  /LD 옵션은 DLL 로 빌드 하겠다는 의미입니다.
+// => CalcProxy.dll 이 생성됩니다.
+
+// [참고] visual studio 의 C++ 컴파일러 이름이 "cl" 입니다.
