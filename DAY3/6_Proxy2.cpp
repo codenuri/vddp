626ab50da07e777150cd3de423fd112677fbb5a1
ecourse <smkang@codenuri.co.kr>
2026-04-29 14:48:25 +0900

    Add files via upload

--- DAY3/6_Proxy2.cpp
+++ DAY3/6_Proxy2.cpp
@@ -54,5 +54,30 @@ int main()
 	// DNS dns;
 	CacheDNS dns;
 
-	std::cout << dns.get_ip("www.samsung.com") << std::endl;
+//	std::cout << dns.get_ip("www.samsung.com") << std::endl;
+	std::cout << dns.get_ip("www.naver.com") << std::endl;
 }
+
+// Proxy 패턴
+// => 다양한 의도로 "기존에 사용하던 클래스" 를 "대신" 하는 클래스를 만드는 것
+
+// 1. 성능 향상을 위한 캐쉬 도입
+// 2. 보안을 위한 proxy - id, pass 검증
+// 3. remote 서버에 대한 대행자 - 다음 예제 
+// 등... 
+
+// 결국 DNS 기능에 "캐쉬" 기능 추가 이므로 "decorator" 패턴 아닌가요 ?
+
+// decorator 패턴 : origin 을 계속 사용할수 있다.
+FileStream fs("a.txt");
+ZipDecorator zd(&fs); // fs 에 압축기능 추가
+
+zd.write("hello");    // 이렇게 하면 기능을 추가해서 사용(압축후 쓰기)
+fs.write("hello");    // 기능 추가 이전의 원래 기능 사용
+
+
+// proxy 패턴 : "기능의 추가" 가 본질이 아니라 "대신하는 것" 을 만드는 것
+//DNS dns;		// 이 클래스 대신
+CacheDNS dns; 	// 이 클래스 사용해라. 
+
+auto ip = dns.get_ip("www.naver.com");
