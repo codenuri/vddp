164dbc27205712218b4407d7352baaa511941591
ecourse <smkang@codenuri.co.kr>
2026-04-29 16:40:56 +0900

    Add files via upload

---
+++ DAY3/7_RemoteProxy7.cpp
@@ -0,0 +1,39 @@
+﻿#define USING_GUI
+#include "cppmaster.h"
+#include "ICalc.h"
+
+// RemoteProxy6.cpp 처럼 참조계수 수명 규칙을 직접 사용하면
+// #1. 규칙 자체가 어렵지는 않지만, 지루한 코드입니다.
+// #2. 실수로 규칙을 어기는 경우가 발생할 수 있습니다.
+
+
+// 참조 계수 규칙을 실수 하지 않도록 만들어 봅시다.
+// => 자동으로 참조계수를 관리하는 스마트 포인터 도입
+// => 안드로이드 오픈소스 안에 아래와 유사한 클래스가 있습니다.
+
+template<typename T> class sp
+{
+	T* ptr;
+public:
+	sp(T* p = nullptr)  : ptr(p)         { if (ptr) ptr->AddRef(); }
+	sp(const sp& other) : ptr(other.ptr) { if (ptr) ptr->AddRef(); }
+
+	~sp() { if (ptr) ptr->Release(); }	
+};
+
+int main()
+{
+	sp<ICalc> calc1 = load_proxy();
+//	calc1->AddRef();	
+
+	sp<ICalc> calc2 = calc1;
+	//calc2->AddRef();	
+
+//	int n1 = calc1->Add(10, 20);
+
+}
+
+
+
+
+
