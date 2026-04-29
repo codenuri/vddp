40556d25f450ec37e28bdbfe2f403a33f0a6f5b3
ecourse <smkang@codenuri.co.kr>
2026-04-29 15:49:16 +0900

    Add files via upload

--- DAY3/CalcProxy.cpp
+++ DAY3/CalcProxy.cpp
@@ -12,3 +12,16 @@ public:
 	int Add(int a, int b) { return ec_send_server(server, 1, a, b); }
 	int Sub(int a, int b) { return ec_send_server(server, 2, a, b); }
 };
+
+// DLL 안에 있는 Proxy 클래스 이름은 
+// => Client 제작자가 알수도 없고, 사용해도 안됩니다(강한결합)
+// => DLL 내부에서 new Calc 해야 합니다
+extern "C" __declspec(dllexport)
+ICalc* create()
+{
+	return new Calc();
+}
+
+// 새로운 Proxy DLL 을 만들어도 
+// => DLL 이름은 동일하게 유지하고
+// => DLL 내부에서 create() 함수 이름도 동일하게 유지. 
