b1cab1e3ddbd4a3213ffc52b9bb6cdb1df502987
ecourse <smkang@codenuri.co.kr>
2026-04-29 15:37:32 +0900

    Add files via upload

--- DAY3/7_RemoteProxy3.cpp
+++ DAY3/7_RemoteProxy3.cpp
@@ -1,7 +1,15 @@
 #define USING_GUI
 #include "cppmaster.h"
 
-class Calc
+struct ICalc
+{
+	virtual int Add(int a, int b) = 0;
+	virtual int Sub(int a, int b) = 0;
+	virtual ~ICalc() {}
+};
+
+
+class Calc : public ICalc 
 {
 	int server;
 public:
@@ -11,9 +19,18 @@ public:
 	int Sub(int a, int b) { return ec_send_server(server, 2, a, b); }
 };
 
+// 위와 같은 Calc Proxy 는 누가 만들게 될까요 ?
+// 1. server 제작자 <= 정답
+// 2. client 제작자
+
 int main()
 {
-	Calc* calc = new Calc();
+	// Client 개발자가 아래처럼 Proxy 이름을 직접 사용하면
+	// 강한 결합(tightly coupling) 이 됩니다.
+	// => 새로운 Proxy 가 등장하면 아래 코드는 수정되어야 합니다
+	// Calc* calc = new Calc();
+
+	// 클라이언트 에서는 Proxy 사용시 인터페이스를 사용한 약한 결합으로 사용해야 합니다
 
 	int n1 = calc->Add(10, 20);
 	int n2 = calc->Sub(10, 20);
