76583c8ffb66f1b451a8254d48130efb69cd4f29
ecourse <smkang@codenuri.co.kr>
2026-04-29 15:32:30 +0900

    Add files via upload

---
+++ DAY3/7_RemoteProxy3.cpp
@@ -0,0 +1,27 @@
+#define USING_GUI
+#include "cppmaster.h"
+
+class Calc
+{
+	int server;
+public:
+	Calc() { server = ec_find_server("Calc"); }
+
+	int Add(int a, int b) { return ec_send_server(server, 1, a, b); }
+	int Sub(int a, int b) { return ec_send_server(server, 2, a, b); }
+};
+
+int main()
+{
+	Calc* calc = new Calc();
+
+	int n1 = calc->Add(10, 20);
+	int n2 = calc->Sub(10, 20);
+
+	std::cout << n1 << ", " << n2 << std::endl;
+}
+
+
+
+
+
