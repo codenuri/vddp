6413c20fafb88365411906596f276df85184c177
ecourse <smkang@codenuri.co.kr>
2026-04-29 09:12:52 +0900

    Add files via upload

---
+++ DAY3/1_command1.cpp
@@ -0,0 +1,28 @@
+#include "monitor.h"
+
+
+// 모니터의 밝기를 변경할때 사용하는 클래스
+class BrightnessCommand
+{
+	Monitor& m;	
+	int value;
+public:
+	BrightnessCommand(Monitor& m, int v) : m(m), value(v) {}
+
+	void Execute()
+	{
+		m.set_brightness(value);
+	}
+};
+
+int main()
+{
+	Monitor m;
+	
+	// 모니터의 밝기를 변경하고 싶다.
+	// #1. m 객체의 멤버함수를 직접 호출
+	m.set_brightness(90);
+
+
+	// #2. command 패턴을 사용하는 방법
+}
