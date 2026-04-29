dfefa712e4dac87992605c04b3e8d3e2f8b9fa35
ecourse <smkang@codenuri.co.kr>
2026-04-29 09:06:54 +0900

    Add files via upload

--- DAY3/0_singleton.cpp
+++ DAY3/0_singleton.cpp
@@ -1,19 +1,24 @@
 ﻿#include <iostream>
 
+// meyer's 싱글톤 코드를 자동 생성하는 매크로
+#define MAKE_SINGLETON(classname) 						\
+private:												\
+	classname() {}										\
+	classname(const classname&) = delete;				\
+	classname& operator=(const classname&) = delete;	\
+public:													\
+	static classname& get_instance() 					\
+	{													\
+		static classname instance;						\
+		return instance;								\
+	}
+
+
 class Cursor
 {
-private:
-	Cursor() {}
-
-	Cursor(const Cursor&) = delete;				
-	Cursor& operator=(const Cursor&) = delete;	
-public:
-	static Cursor& get_instance() 
-	{
-		static Cursor instance;
-		return instance;
-	}
+	MAKE_SINGLETON(Cursor)
 };
+
 int main()
 {
 	Cursor& c1 = Cursor::get_instance();
